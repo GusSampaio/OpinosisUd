2bac60bc41d59edfa80804f940d79d4c192ddaf3
GusSampaio <gustavo.sampaio@usp.br>
2025-01-25 13:15:05 +0000

    Adding git ignore

--- com/opinosis/OpinosisMain.java
+++ com/opinosis/OpinosisMain.java
@@ -59,7 +59,6 @@ public class OpinosisMain extends OpinosisSettings {
     }
 
     public static void main(String[] args) {
-        System.out.println("Novo teste1");
         OpinosisMain main = new OpinosisMain();
         main.start(args);
     }
